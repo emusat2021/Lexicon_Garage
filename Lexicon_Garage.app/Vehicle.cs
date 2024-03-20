ef0d3605c18d28d3be5e0700cda3c5e070d811f8
Eugenia <musat.eugenia@gmail.com>
2024-03-20 16:19:40 +0100

    Add Vehicle that will inherit Iterface IVehicle

---
+++ Lexicon_Garage.app/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_Garage.app
+{
+    internal class Vehicle : IVehicle
+    {
+        private readonly string _registrationNumber;
+        private string _color;
+        private int _numberOfWheels;
+        private int _numberOfSets;
+        private string _fueltype;
+
+        public string RegistrationNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Color { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int NumberOfWheels { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int NumberOfSets { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Fueltype { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Vehicle()
+        {
+        }
+
+        public Vehicle(int numberOfWheels, string registrationNumber, string color, int numberOfSets, string fueltype)
+        {
+            this._numberOfWheels = numberOfWheels;
+            this._registrationNumber = registrationNumber;
+            this._color = color;
+            this._numberOfSets = numberOfSets;
+            this._fueltype = fueltype;
+        }
+
+
+    }
+}
