1ebecd64411dfca2da236877cc92c19d7e724af9
Eugenia <musat.eugenia@gmail.com>
2024-03-21 15:46:45 +0100

    Create Handler and extract Interface from it

---
+++ Lexicon_Garage.app/Handler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_Garage.app
+{
+    internal class Handler : IHandler
+    {
+        private int _capacity;
+        public int Capacity { get; set; }
+
+        public Garage<T> CreateGarage<T>() where T : IVehicle
+        {
+            return new Garage<T>(Capacity);
+        }
+    }
+}
---
+++ Lexicon_Garage.app/IHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Lexicon_Garage.app
+{
+    internal interface IHandler
+    {
+        int Capacity { get; set; }
+
+        Garage<T> CreateGarage<T>() where T : IVehicle;
+    }
+}
