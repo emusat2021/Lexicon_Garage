5e2ea36a50a63e4b399d12acec3bc5bb4cf55e4d
Eugenia <musat.eugenia@gmail.com>
2024-03-22 16:38:57 +0100

    Refactor interface IVehicle

--- Lexicon_Garage.app/IVehicle.cs
+++ Lexicon_Garage.app/IVehicle.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Lexicon_Garage.app
+﻿namespace Lexicon_Garage.app
 {
     public interface IVehicle
     {
-
-        string RegistrationNumber { get; set; }
+        string Model { get; set; }
         string Color { get; set; }
-        int NumberOfWheels { get; set; }
-        int NumberOfSets { get; set; }
         string Fueltype { get; set; }
-        
+        int NumberOfSets { get; set; }
+        int NumberOfWheels { get; set; }
+        string RegistrationNumber { get; set; }
+
+        string Stats();
     }
 }
