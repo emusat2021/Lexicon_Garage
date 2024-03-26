d976b24cc6342d2e800e5b5d4133e1215efd38b6
Eugenia <musat.eugenia@gmail.com>
2024-03-26 11:47:23 +0100

    Create UI and Interface to it

---
+++ Lexicon_Garage.app/UserInterface/IUI.cs
@@ -0,0 +1,11 @@
+﻿namespace Lexicon_Garage.app.UserInterface
+{
+    public interface IUI
+    {
+        string GetInput();
+
+        void Clean();
+
+        void Print(string message);
+    }
+}
---
+++ Lexicon_Garage.app/UserInterface/UI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Lexicon_Garage.app.UserInterface;
+
+namespace Lexicon_Garage.app
+{
+    public class UI : IUI
+    {
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string GetInput()
+        {
+            return Console.ReadLine()!;
+        }
+
+        public void Clean()
+        {
+            Console.Clear();
+        }
+
+    }
+}
